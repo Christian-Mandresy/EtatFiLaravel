e54af7f772cf20ce9ee8ad15db3b61b3315e4969
Christian-Mandresy <89293572+Christian-Mandresy@users.noreply.github.com>
2024-08-20 19:23:13 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\EmployerController;
 */
 Route::post('register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
